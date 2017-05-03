f9ddbfc923e7de3a631d6fea5e62372a164a3288
Aleksander Rendtslev <aleksander@rendtslev.dk>
2017-05-03 10:33:23 +0200

    Added build folder to gitignore

--- build/server.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
-var app = express();
-app.listen(8000, function () { return console.log("Server running on 8000!"); });
-//# sourceMappingURL=server.js.map
